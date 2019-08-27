c7854cd80229636966c3843015d29f5c05953273
lzy960601 <21952652+lzy960601@users.noreply.github.com>
2019-08-27 15:52:45 +0800

    .

--- Models/StatisticsFeature.py
+++ Models/StatisticsFeature.py
@@ -11,72 +11,52 @@ people_region_dict = {
 class SF:
     def __init__(self):
         self.df = pd.read_csv(filepath, dtype=int)
-        self.dic = {}
-        self.dic['uid'] = {}
-        self.dic['author_id'] = {}
-        self.area_average = {}
-        self.PreProcess()
-
-    def update(self, row, name):
-        key = row[name]
-        if not key in self.dic[name].keys():
-            self.dic[name][key] = {}
-            self.dic[name][key]['area'] = row[people_region_dict[name]]
-            for types in ['like', 'finish']:
-                self.dic[name][key][types] = {
-                    'count' : 0,
-                    'action' : 0,
-                    'sum_of_duration' : 0
-                }
-        for types in ['like', 'finish']:
-            self.dic[name][key][types]['count'] += 1
-            if row[types] == 1:
-                self.dic[name][key][types]['action'] += 1
-                self.dic[name][key][types]['sum_of_duration'] += row['duration']
-
-    def update(self, name, area, types, rat, avg):
-        self.area_average[name][area][types]['ratio'] *= self.area_average[name][area][types]['count']
-        self.area_average[name][area][types]['average'] *= self.area_average[name][area][types]['average']
-        self.area_average[name][area][types]['ratio'] += rat
-        self.area_average[name][area][types]['average'] += avg
-        self.area_average[name][area][types]['count'] += 1.0
-        self.area_average[name][area][types]['ratio'] /= self.area_average[name][area][types]['count']
-        self.area_average[name][area][types]['average'] /= self.area_average[name][area][types]['count']
+        self.area = {}
 
     def PreProcess(self):
-        for row in self.df.rows:
-            self.update(row, 'uid')
-            self.update(row, 'author_id')
-        for name in ['uid', 'authod_id']:
-            self.area_average[name] = {}
-            for key in range(2):
-                self.area_average[name][key] = {}
-                for types in ['like', 'finish']:
-                    self.area_average[name][key][types] = {}
-                    for dat in ['ratio', 'average', 'count']:
-                        self.area_average[name][key][types][dat] = 0.0
-            for key in self.dic[name].keys():
-                for types in ['like', 'finish']:
-                    tmp_dict = self.dic[name][key][types]
-                    self.dic[name][key][types]['ratio'] = float(tmp_dict['action']) / float(tmp_dict['count'])
-                    self.dic[name][key][types]['average'] = float(tmp_dict['sum_of_duration']) / float(tmp_dict['count'])
-                    self.update(
-                        name,
-                        self.dic[name][key]['area'],
-                        types,
-                        self.dic[name][key][types]['ratio'],
-                        self.dic[name][key][types]['average']
-                    )
+        for name in ['uid', 'author_id']:
+            self.area[name] = {}
+            for types in ['finish', 'like']:
+                self.area[name][types] = {}
+                for id in range(2):
+                    self.area[name][types][id] = {}
+                    # ratio
+                    self.area[name][types][id]['ratio'] = float(
+                        self.df[self.df[people_region_dict[name]] == id & self.df[types] == 1].shape[0]) / float(
+                        self.df[self.df[people_region_dict[name]] == id].shape[0])
+                    # average
+                    self.area[name][types][id]['ratio'] = float(
+                        self.df[self.df[people_region_dict[name]] == id & self.df[types] == 1].apply(sum)['duration']) / float(
+                        self.df[self.df[people_region_dict[name]] == id].shape[0])
+
+    def statics(self, row, name, types):
+        ret = []
+        # ratio
+        try:
+            if self.df[name == row[name]].shape[0] > limit:
+                ret.append(float(self.df[self.df[name] == row & self.df[self.df[types]] == 1].shape[0]) / float(self.df[self.df[name] == row[name]].shape[0]))
+            else:
+                ret.append(self.area[row[people_region_dict[name]]]['ratio'])
+        except Exception as ex:
+            ret.append(self.area[row[people_region_dict[name]]]['ratio'])
+
+        # average
+        try:
+            if self.df[name == row[name]].shape[0] > limit:
+                ret.append(float(self.df[self.df[name] == row & self.df[self.df[types]] == 1].apply(sum)['duration']) / float(
+                    self.df[self.df[name] == row & self.df[self.df[types]] == 1].shape[0]))
+            else:
+                ret.append(self.area[name][types][row[people_region_dict[name]]]['ratio'])
+        except Exception as ex:
+            ret.append(self.area[name][types][row[people_region_dict[name]]]['ratio'])
+        return ret
 
     def GetResult(self, row):
         F = []
         for name in ['uid', 'author_id']:
             for types in ['finish', 'like']:
-                for results in ['ratio', 'average']:
-                    if not row[name] in self.dic[name].keys() or self.dic[name][row[name]][types]['count'] <= limit:
-                        F.append(self.area_average[name][people_region_dict[name]][types][results])
-                    else:
-                        F.append(self.dic[name][row[name]][types][results])
+                for val in self.statics(row, name, types):
+                    F.append(val)
         return F
 
 def test():
